06431dd0ca41c5c08524b5f4b1ffb47a702fba9e
MediaLab <MediaLab@DESKTOP-7KAL228>
2023-09-21 14:53:26 +0900

    project 09_21

---
+++ hello/src/hello/WhileSample.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class WhileSample {
+
+	public static void main(String[] args) {
+		char c = 'a';
+		
+		while (c <= 'z') {
+			System.out.print(c);
+			c = (char)(c+1);
+		}
+
+	}
+
+}
